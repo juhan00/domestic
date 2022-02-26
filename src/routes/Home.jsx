c37c578574a3b7e4714d0e0a20332b1fd24e8e24
Kimham <choiwook940@naver.com>
2022-02-27 01:36:08 +0900

    feat: Make UI of disclosure selection box
    공시 컴포넌트 생성 후 공시 정렬 기준 Form 작성
    공시 리스트 UI 작업 후 Form 서브밋 로직 작성 예정

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,7 +1,10 @@
 import React from "react";
+import Disclosure from "../components/disclosure"; // 공시 페이지 임시 랜더링
 
 const Home = () => {
-  return <body>test</body>;
+  return <body>test
+    <Disclosure /> {/* 공시 페이지 임시 랜더링 */}
+  </body>;
 };
 
 export default Home;
