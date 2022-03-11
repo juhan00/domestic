644c00289b0c12aa895f75a3149bac16cd3bfc32
ParkSungHyun <psh1030.dev@gmail.com>
2022-03-12 00:28:36 +0900

    Design: modify keywordchart padding

--- src/components/KeywordChart/style.js
+++ src/components/KeywordChart/style.js
@@ -2,7 +2,7 @@ import styled from "@emotion/styled";
 
 export const KeywordChartWrapper = styled.div`
   width: 100%;
-  padding: 0px 5%;
+  padding: 0px 30%;
   min-height: 300px;
   box-sizing: border-radius;
   & > svg {
