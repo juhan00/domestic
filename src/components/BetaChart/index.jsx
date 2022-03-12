528c6816e70645f527b65f5f71ccf69a2231884e
GyoHeonLee <ghl12078@gmail.com>
2022-03-12 16:28:59 +0900

    Feat: beta chart add resize and tooltip

--- src/components/BetaChart/index.jsx
+++ src/components/BetaChart/index.jsx
@@ -1,79 +1,75 @@
 import React, { useRef, useEffect, useState, useCallback } from "react";
-import { ChartWrapper } from "./style";
-import {
-  axisBottom,
-  axisLeft,
-  extent,
-  max,
-  min,
-  scaleLinear,
-  scaleTime,
-  select,
-  line,
-} from "d3";
-import { reduce } from "d3";
+import { ChartWrapper, InputWrapper, Header, YLabel } from "./style";
+import Tooltip from "./Tooltip";
+import Search from "@components/Search";
+import * as d3 from "d3";
+import useResizeObserver from "@utils/useResizeObserver";
 
-const BetaChart = ({
-  data,
-  names,
-  width = 1200,
-  height = 501,
-  margin = { top: 187, bottom: 95, left: 77.5, right: 32.5 },
-  padding = 32,
-}) => {
-  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
-
-  const betaChartRef = useRef();
+const BetaChart = ({ data, names }) => {
+  const betaChartRef = useRef(null);
   const svgRef = useRef(null);
   const tooltipRef = useRef(null);
+  const resizeWidth = useResizeObserver(betaChartRef);
+
+  const [hoveredValue, setHoveredValue] = useState(null);
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
 
   const handleMouseMove = useCallback(
     (event) => {
-      const { clientX, clientY } = event;
-      setMousePosition({ x: clientX, y: clientY });
+      const { offsetX, offsetY } = event;
+      setMousePosition({ x: offsetX, y: offsetY });
     },
     [setMousePosition],
   );
 
+  // initial setting
   useEffect(() => {
-    const betaChartWrapper = select(betaChartRef.current);
-    const svg = select(svgRef.current);
-    const tooltip = select(tooltipRef.current);
+    const betaChartWrapper = d3.select(betaChartRef.current);
+    const svg = d3.select(svgRef.current);
+    const tooltip = d3.select(svgRef.current);
 
-    // xAxis
-    const maxValueX = max(data, (data) => data.xPrice);
-    const minValueX = min(data, (data) => data.xPrice);
+    svg.selectAll(".dots").remove();
 
-    const xScale = scaleLinear()
+    if (!resizeWidth || !data) {
+      return;
+    }
+    const margin = { top: 20, right: 30, bottom: 30, left: 60 };
+    const width = resizeWidth.width - (margin.left + margin.right);
+    const height = 450;
+    const xTickCount = 12;
+    const yTickCount = 7;
+    const xTickBlankCount = 0;
+    const maxValueX = d3.max(data, (data) => data.xPrice);
+    const minValueX = d3.min(data, (data) => data.xPrice);
+    const maxValueY = d3.max(data, (data) => data.yPrice);
+    const minValueY = d3.min(data, (data) => data.yPrice);
+
+    const xScale = d3
+      .scaleLinear()
       .domain([
         minValueX - Math.abs(minValueX) * 0.05,
         maxValueX + Math.abs(maxValueX) * 0.05,
       ])
       .range([margin.left, width - margin.right])
       .nice();
-
     svg
       .append("g")
       .attr("transform", `translate(0,${height})`)
-      .call(axisBottom(xScale))
+      .call(d3.axisBottom(xScale))
       .call((g) => g.selectAll(".tick line").remove());
 
-    // yAxis
-    const maxValueY = max(data, (data) => data.yPrice);
-    const minValueY = min(data, (data) => data.yPrice);
-
-    const yScale = scaleLinear()
+    const yScale = d3
+      .scaleLinear()
       .domain([
         minValueY - Math.abs(minValueY) * 0.05,
         maxValueY + Math.abs(maxValueY) * 0.05,
       ])
       .range([height, margin.bottom])
       .nice();
-
     svg
       .append("g")
       .attr("transform", `translate(${margin.left}, 0)`)
-      .call(axisLeft(yScale))
+      .call(d3.axisLeft(yScale))
       .call((g) => g.select(".domain").remove())
       .call((g) => g.selectAll(".tick line").remove());
 
@@ -88,11 +84,6 @@ const BetaChart = ({
       .style("border-radius", "5px")
       .style("padding", "10px");
 
-    const mouseover = (e, d) => {
-      tooltip.transition().duration(200).style("visibility", "visible");
-      // circle.style("background-color", "black");
-      console.log(e);
-    };
     const mousemove = (e, d) => {
       tooltip
         .html(
@@ -103,34 +94,57 @@ const BetaChart = ({
           `translate(${e.offsetX - 30}px, ${e.offsetY - 20}px)`,
         );
     };
-    const mouseleave = (e, d) => {
-      tooltip.transition().duration(200).style("visibility", "hidden");
-      svg.style("background-color", "white");
-    };
 
     // dots
     svg
-      .append("g")
-      .selectAll("dot")
+      .selectAll(".dot")
       .data(data)
-      .enter()
-      .append("circle")
-      .attr("cx", (d) => xScale(d.xPrice))
-      .attr("cy", (d) => yScale(d.yPrice))
-      .attr("r", 5)
-      .style("fill", "#f6a64b")
-      .on("mouseover", mouseover)
-      .on("mousemove", mousemove)
-      .on("mouseleave", mouseleave);
-  }, [data]);
+      .join((enter) => {
+        const dots = enter.append("g").classed("dots", true);
+
+        dots
+          .append("circle")
+          .attr("cx", (d) => xScale(d.xPrice))
+          .attr("cy", (d) => yScale(d.yPrice))
+          .attr("r", 5)
+          .style("fill", "#f6a64b")
+          .on("mouseover", (e, d) => {
+            setHoveredValue(d);
+            console.log(d);
+          })
+          .on("mousemove", handleMouseMove)
+          .on("mouseleave", () => setHoveredValue(null));
+      });
+  }, [data, resizeWidth]);
 
   return (
-    <ChartWrapper ref={betaChartRef}>
-      <div ref={tooltipRef} />
-      <svg ref={svgRef}>
-        <g className="x-axis" />
-        <g className="y-axis" />
-      </svg>
+    <ChartWrapper>
+      <Header>
+        <InputWrapper>
+          <input type="date" name="date" />
+          ~
+          <input type="date" name="date" />
+        </InputWrapper>
+        <Search />
+        <YLabel>
+          TICKER Y-AXIS<span>{names[1]}</span>
+        </YLabel>
+      </Header>
+
+      <h5>
+        {names[0]} (X) vs {names[1]} (Y)
+      </h5>
+      <div className="betaChartRef" ref={betaChartRef}>
+        <Tooltip
+          hoveredValue={hoveredValue}
+          mousePosition={mousePosition}
+          names={names}
+        />
+        <svg ref={svgRef}>
+          <g className="x-axis" />
+          <g className="y-axis" />
+        </svg>
+      </div>
     </ChartWrapper>
   );
 };
