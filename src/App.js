b275566eaa8051ed3d682780d228232cb6f8ef92
05Castle <kos4005@gmail.com>
2022-03-16 05:17:51 +0900

    Feat: scroll to top when route location change

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import { BrowserRouter } from "react-router-dom";
 import GlobalStyle from "./GlobalStyle";
 import React from "react";
 import Router from "./Router";
+import ScrollToTop from "@utils/scrollToTop";
 
 const App = () => {
   return (
     <BrowserRouter>
       <GlobalStyle />
+      <ScrollToTop />
       <Router />
     </BrowserRouter>
   );
