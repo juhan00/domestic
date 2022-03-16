817a456e48b93f9f552148be53a29ef5a80a803b
ParkSungHyun <psh1030.dev@gmail.com>
2022-03-16 13:09:23 +0900

    Feat: add chart

--- src/components/ComInfoGraph/index.jsx
+++ src/components/ComInfoGraph/index.jsx
@@ -1,8 +1,41 @@
-import React from "react";
+import React, { useReducer } from "react";
 import { GraphWrapper } from "./style";
+import {
+  reducer,
+  initialState,
+  setPeriod,
+} from "@reducers/domesticfinance/domesticfinance";
+import MainAreaChart from "@components/MainAreaChart";
+import MainChart from "@components/MainChart";
+
+const publicDate = new Date("2021-08-01");
 
 const DoFinancialGraph = () => {
-  return <GraphWrapper>Graph</GraphWrapper>;
+  const [domesticState, dispatch] = useReducer(
+    reducer,
+    initialState(publicDate),
+  );
+
+  const { period, stockData } = domesticState;
+  const { startDate, endDate, diff } = period;
+
+  return (
+    <GraphWrapper>
+      {diff < 91 && startDate && endDate ? (
+        <MainChart
+          startDate={startDate}
+          endDate={endDate}
+          stockData={stockData}
+        />
+      ) : (
+        <MainAreaChart
+          startDate={startDate}
+          endDate={endDate}
+          stockData={stockData}
+        />
+      )}
+    </GraphWrapper>
+  );
 };
 
 export default DoFinancialGraph;
