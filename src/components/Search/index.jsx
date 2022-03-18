a67d43a06339495b9d482050202ffe16002c5cbf
05Castle <kos4005@gmail.com>
2022-03-18 15:20:45 +0900

    Feat: x-ticker search list separated by domestic/global

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -2,11 +2,14 @@ import React, { useRef, useState, useEffect, useMemo } from "react";
 import { clickOutside } from "@utils/clickOutside";
 import { SearchMenuStyleOnHeader } from "./style";
 import axios from "redaxios"
+import { useLocation } from "react-router-dom";
 
 export const Search = () => {
   const ref = useRef();
+  const location = useLocation().pathname
   const [searchItem, setSearchItem] = useState("");
   const [domesticList, setDomesticList] = useState([]);
+  const [globalList, setGlobalList] = useState([]);
   const [isOpen, setIsOpen] = useState(false);
   const handleItem = (e) => {
     setSearchItem(e.target.value);
@@ -18,8 +21,12 @@ export const Search = () => {
       const domesticData = await axios.get(
         "https://gyoheonlee.github.io/mobile-bank/data/api/doList.json",
       );
+      const globalData = await axios.get(
+        "https://gyoheonlee.github.io/mobile-bank/data/api/goList.json",
+      );
       if (isComponentMounted) {
         setDomesticList(domesticData.data.domestic);
+        setGlobalList(globalData.data.global);
       }
     };
     fetchData();
@@ -40,6 +47,18 @@ export const Search = () => {
     });
     return filtered;
   }, [searchItem, domesticList]);
+  const globalFiltered = useMemo(() => {
+    let filtered = [];
+    globalList.filter((list) => {
+      if (
+        list.symbol.toLowerCase().includes(searchItem.toLowerCase()) ||
+        list.companyName.toLowerCase().includes(searchItem.toLowerCase())
+      ) {
+        filtered.push(list);
+      }
+    });
+    return filtered;
+  }, [searchItem, globalList]);
 
   const handleClick = (id) => {
     setIsOpen(false);
@@ -49,10 +68,18 @@ export const Search = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (domesticFiltered.length >= 1) {
-      console.log(domesticFiltered[0].crno);
-    } else if (domesticFiltered.length === 0 ){
+    if (location.includes("domestic")) {
+      if (domesticFiltered.length >= 1) {
+      console.log(domesticFiltered[0].crno);       
+      } else if(domesticFiltered.length===0) {
+        null
+      }
+    } else if (location.includes("global")) {
+      if (globalFiltered.length >= 1) {
+      console.log(globalFiltered[0].crno);       
+    } else if(globalFiltered.length===0) {
       null
+      }
     }
   }
 
@@ -74,28 +101,20 @@ export const Search = () => {
         </div>
         <div
           className={isOpen ? "seachResultWrapper" : "seachResultWrapper hide"}>
-          <ul className="searchResultList">
-            {domesticList
-              .filter((list) => {
-                if (searchItem == "") {
-                  return list;
-                } else if (
-                  list.crno
-                    .toLowerCase()
-                    .includes(searchItem.toLowerCase()) ||
-                  list.itmsNm
-                    .toLowerCase()
-                    .includes(searchItem.toLowerCase())
-                ) {
-                  return list;
-                }
-              })
-              .map((list, index) => (
+            {location.includes('domestic') ? <ul className="searchResultList">
+            {domesticFiltered.map((list, index) => (
                 <li className="serachResultItem" key={index} onClick={()=>handleClick(list.crno)}>
                   <span>{list.itmsNm}</span> | {list.crno}
                 </li>
               ))}
-          </ul>
+          </ul>: <ul className="searchResultList">
+            {globalFiltered.map((list, index) => (
+                <li className="serachResultItem" key={index} onClick={()=>handleClick(list.symbol)}>
+                  <span>{list.symbol}</span> | {list.companyName}
+                </li>
+              ))}
+          </ul>}
+          
         </div>
       </div>
     </SearchMenuStyleOnHeader>
