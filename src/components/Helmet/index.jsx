c22d2bec55cabf4ee6e655a1cb04abe4d51a6d90
ParkSungHyun <psh1030.dev@gmail.com>
2022-03-20 18:14:24 +0900

    Fix: modify helmet error

--- src/components/Helmet/index.jsx
+++ src/components/Helmet/index.jsx
@@ -1,29 +1,31 @@
 import React from "react";
-import { Helmet } from "react-helmet";
+import { Helmet, HelmetProvider } from "react-helmet-async";
 
 const HelmetCom = () => {
   return (
     <div className="application">
-      <Helmet>
-        <meta charSet="utf-8" />
-        <title>Moya for Desktop</title>
+      <HelmetProvider>
+        <Helmet>
+          <meta charSet="utf-8" />
+          <title>Moya for Desktop</title>
 
-        <meta
-          name="description"
-          content="내 책상 위의 애널리스트, MoYa for Desktop"
-        />
+          <meta
+            name="description"
+            content="내 책상 위의 애널리스트, MoYa for Desktop"
+          />
 
-        <meta property="og:type" content="website" />
-        <meta property="og:title" content="Moya for Desktop" />
-        <meta property="og:site_name" content="Moya" />
+          <meta property="og:type" content="website" />
+          <meta property="og:title" content="Moya for Desktop" />
+          <meta property="og:site_name" content="Moya" />
 
-        <link
-          rel="icon"
-          type="image/x-icon"
-          href="/images/favicon.ico"
-          sizes="16x16"
-        />
-      </Helmet>
+          <link
+            rel="icon"
+            type="image/x-icon"
+            href="/images/favicon.ico"
+            sizes="16x16"
+          />
+        </Helmet>
+      </HelmetProvider>
     </div>
   );
 };
